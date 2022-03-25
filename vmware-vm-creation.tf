7dbb113f1c06906ce0829f66099f02d3564194b2
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2022-03-25 09:50:55 +0100

    Add files via upload

---
+++ vmware-vm-creation.tf
@@ -0,0 +1,37 @@
+# Vm creation
+resource "vsphere_virtual_machine" "vm" {
+  depends_on       = [module.aci]
+  for_each         = var.vms
+  name             = format("%s-%s",var.tenant, each.value.name)
+  resource_pool_id = data.vsphere_resource_pool.esx[each.value.esx].id
+  datastore_id     = data.vsphere_datastore.datastore_NetApp.id
+  num_cpus   = 1
+  memory     = 1024
+  wait_for_guest_net_timeout = 0
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+  network_interface {
+    network_id   = data.vsphere_network.pg[each.value.zone].id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+  disk {
+   label            = "disk0"
+   size             = data.vsphere_virtual_machine.template.disks.0.size
+   eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+   thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = format("%s-%s",var.tenant, each.value.name)
+        domain    = "cisco.com"
+      }
+      network_interface {
+        ipv4_address = each.value.ip
+        ipv4_netmask = each.value.mask
+      }
+      ipv4_gateway = each.value.gw
+    }
+  }
+}
